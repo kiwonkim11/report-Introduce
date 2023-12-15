cf2eeb88f7cd72cc069587af25c921f982ceec9b
kiwon kim <rlarldnjs17@gmail.com>
2023-12-15 12:11:23 +0900

    자기소개 앱 Lv. 3 완성

--- app/src/main/java/com/example/introduce/HomeActivity.kt
+++ app/src/main/java/com/example/introduce/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.introduce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val idData = intent.getStringExtra("id")
+        val seeIdData = findViewById<TextView>(R.id.txt_id_confirm)
+        val btnFinish = findViewById<Button>(R.id.btn_finish)
+
+        seeIdData.setText(idData)
+        btnFinish.setOnClickListener {
+            finish()
+        }
     }
 }
